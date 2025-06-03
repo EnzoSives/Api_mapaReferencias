b874cd81eacd50c08ceb4b0e263db2855c071867
Enzo Sives <enzosives7@gmail.com>
2025-06-02 22:26:53 -0300

    Editar marcadores

--- src/marcador/marcador.service.ts
+++ src/marcador/marcador.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Marcador } from './entities/marcador.entity';
+import { IntegranteFamilia } from '../integrante_familiar/entities/integrante_familiar.entity';
 import { Repository } from 'typeorm';
 
 @Injectable()
@@ -8,6 +9,9 @@ export class MarcadorService {
   constructor(
     @InjectRepository(Marcador)
     private marcadorRepo: Repository<Marcador>,
+
+    @InjectRepository(IntegranteFamilia)
+    private integranteRepo: Repository<IntegranteFamilia>, // 👈 agregado
   ) {}
 
   create(data: Partial<Marcador>) {
@@ -26,8 +30,36 @@ export class MarcadorService {
     });
   }
 
-  update(id: number, data: Partial<Marcador>) {
-    return this.marcadorRepo.update(id, data);
+  async update(id: number, data: Partial<Marcador>) {
+    const marcador = await this.marcadorRepo.findOne({
+      where: { id },
+      relations: ['integrantes'],
+    });
+
+    if (!marcador) {
+      throw new NotFoundException('Marcador no encontrado');
+    }
+
+    const { integrantes, ...resto } = data;
+
+    // Actualiza campos simples
+    Object.assign(marcador, resto);
+
+    if (integrantes) {
+      // Borra integrantes anteriores
+      await this.integranteRepo.delete({ marcador: { id } });
+
+      // Obtiene el marcador completo para asignar a los integrantes
+      const marcadorCompleto = await this.marcadorRepo.findOne({ where: { id } });
+
+      // Asocia los nuevos integrantes al marcador
+      marcador.integrantes = integrantes.map((i) => ({
+        ...i,
+        marcador: marcadorCompleto,
+      }));
+    }
+
+    return this.marcadorRepo.save(marcador);
   }
 
   remove(id: number) {
